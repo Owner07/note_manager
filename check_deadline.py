e5e1b74b1bbe93a4f63fe89db72f0fdc2a7306f9
Owner07 <makoveev_2012@mail.ru>
2025-01-15 22:11:15 +0500

    ..

--- check_deadline.py
+++ check_deadline.py
@@ -2,7 +2,7 @@ from datetime import datetime # импорт класса для работы с
 
 DATE_FORMAT = "%d.%m.%Y"  # Константа для формата даты
 
-print("Текущая дата: "+datetime.now().strftime("%d.%m.%Y"))
+print("Текущая дата: " + datetime.now().strftime("%d.%m.%Y"))
 def get_deadline_date():
     # Запрашивает у пользователя дату дедлайна и возвращает объект datetime.
     while True:
@@ -26,17 +26,17 @@ def склонять_день(days):
 
 def проверить_дедлайн():
     # Проверяет, истек ли дедлайн, и выводит результат.
-    issue_date = get_deadline_date() # Вызываем функцию ввода даты
+    issue_date = get_deadline_date()
 
     now_date = datetime.now() # присваивает дату на момент запроса
 
     if issue_date.date() < now_date.date():
         days_overdue = (now_date - issue_date).days
-        print(f"Дедлайн истек на {days_overdue} {склонять_день(days_overdue)} назад.") # высчитываем разницу между датами
+        print(f"Дедлайн истек  {days_overdue} {склонять_день(days_overdue)} назад.") # высчитываем разницу между датами
     elif issue_date.date() == now_date.date():
         print("Дедлайн сегодня!")
     else:
-        days_remaining = (issue_date.date() - now_date.date()).days
+        days_remaining = (issue_date - now_date).days
         print(f"Дедлайн еще не истек. Осталось {days_remaining} {склонять_день(days_remaining)}.")  # высчитываем разницу между датами
 
 
