7f7333a92282e3e3bf3c2bb483694e7c8264e849
Owner07 <makoveev_2012@mail.ru>
2025-01-22 16:29:58 +0500

    ...

--- search_notes_function.py
+++ search_notes_function.py
@@ -4,27 +4,21 @@ from colorama import Fore, Style
 
 colorama.init(autoreset=True)
 
-
-def i(date):
-    return datetime.strptime(date['issue_date'], '%d-%m-%Y')
-
-
 def display_note(note, index):
-    """Выводит информацию о заметке."""
     print(Fore.BLUE + f"\nЗаметка №{index}:" + Style.RESET_ALL)
     print(Fore.MAGENTA + "    Имя:" + Style.RESET_ALL, note['username'])
     print(Fore.MAGENTA + "    Заголовок:" + Style.RESET_ALL, note['title'])
     print(Fore.MAGENTA + "    Описание:" + Style.RESET_ALL, note['content'])
     print(Fore.MAGENTA + "    Статус:" + Style.RESET_ALL, note['status'])
     print(Fore.MAGENTA + "    Дата создания:" + Style.RESET_ALL, note['created_date'])
-    print(Fore.MAGENTA + "    Дедлайн:" + Style.RESET_ALL, note['issue_date'])
+    print(Fore.MAGENTA + "    Дедлайн:" + Style.RESET_ALL, note['issue_date'])  # функция вывода найденных заметок
 
 
 def check_keyword_in_note(note, keyword):
-    # проверка содержится ли поисковое слово в username, title или content заметки
+
     return (keyword in note["username"].lower() or
             keyword in note["title"].lower() or
-            keyword in note["content"].lower())
+            keyword in note["content"].lower()) # проверка содержится ли поисковое слово в username, title или content заметки
 
 
 def search_notes(notes, keyword=None, status=None):
@@ -38,21 +32,21 @@ def search_notes(notes, keyword=None, status=None):
         keyword = ""
     if not status:
         status = ""
-    keyword = keyword.lower()
-    status = status.lower()
-    for i, note in enumerate(notes):
+    keyword = keyword.lower().strip()
+    status = status.lower().strip()  # перевод в нижний регистр и устранение пробелов если бы был инпут
+    for i, note in enumerate(notes): # нумерация заметок
         if keyword and status:
             if check_keyword_in_note(note, keyword) and status == note["status"].lower():
                 found_notes.append(note)
-                display_note(note, i)
+                display_note(note, i) # сначала поиск самого сложного варианта по 2 значениям
         elif keyword:
             if check_keyword_in_note(note, keyword):
                 found_notes.append(note)
-                display_note(note, i)
+                display_note(note, i) # поиск по слову
         elif status:
-            if status == note["status"].lower():
+            if status == note["status"].lower().strip():
                 found_notes.append(note)
-                display_note(note, i)
+                display_note(note, i) # поиск по статусу
 
     if not found_notes and (keyword or status):
         print(Fore.RED + "\nЗаметки, соответствующие запросу, не найдены." + Style.RESET_ALL)
@@ -60,7 +54,7 @@ def search_notes(notes, keyword=None, status=None):
     return found_notes
 
 
-# Пример использования:
+
 if __name__ == "__main__":
     notes = [
         {
