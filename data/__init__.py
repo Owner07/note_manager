b8a7ece7f80b82dafa73aee63bd09292393a2032
Owner07 <makoveev_2012@mail.ru>
2025-01-31 14:31:34 +0500

    ...1

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,5 @@
-
+from .create_note_function import create_note
+from .save_notes_to_file import save_notes_to_file
+from .load_notes_from_file import load_notes_from_file
+from .append_notes_to_file import append_notes_to_file
+from .save_notes_json import save_notes_json
